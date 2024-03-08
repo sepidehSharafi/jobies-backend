db3a28d6e7744809cde3eee57f20f45421c381f1
Sepideh <sepidehsharafi200@gmail.com>
2024-03-08 13:49:52 +0330

    sign up fixed

--- index.js
+++ index.js
@@ -16,33 +16,50 @@ const port = 3000;
 
 
 app.get('/users', async (_, response) => {
+    try{
     const tweets = await sql`select * from users`;
-    response.send(tweets);
+    response.send(tweets);}
+    catch(e){
+        response.status(500).send({ error: e.message });
+    }
 });
 
-app.get('/posts', async (_, response) => {
+app.get('/posts', async (request, response) => {
+    try{
     const tweets = await sql`select * from posts`;
     response.send(tweets);
+    }catch(e){
+        response.status(500).send({ error: e.message });
+    }
 });
 
 app.post('/login', async (request, response) => {
-    const { username, password } = request.body;
-    const findUser = await sql`SELECT * FROM users WHERE username = ${username} AND password = ${password};`;
-    if (findUser && findUser.length > 0) {
-        response.send({ user: findUser[0] });
-    } else {
-        response.status(401).send({ error: true, message: 'Wrong username or password!' });
+    try {
+        const { username, password } = request.body;
+        const findUser = await sql`SELECT * FROM users WHERE username = ${username} AND password = ${password};`;
+        if (findUser && findUser.length > 0) {
+            console.log(findUser);
+                    const {userid, firstname, lastname, password, username, email } = user
+
+            response.send({ user: {userid: id, firstname, lastname, password, username, email} });
+        } else {
+            response.status(401).send({ error: true, message: 'Wrong username or password!' });
+        }
+    } catch (e) {
+        response.status(500).send({ error: e.message });
     }
 });
 
 app.post('/signup', async (request, response) => {
     try {
         const user = request.body;
+        console.log("user: ", user);
+        const {firstname, lastname, password, username, email } = user
         await sql`INSERT INTO users (firstname, lastname, username, password, email) VALUES
-        ('${user.firstname}','${user.lastname}','${user.username}','${user.password}','${user.email}');`;
-        // await sql(sqlUser);
-            response.status(201).send({ user: { firstname, lastname, username, password, email } });
+        (${firstname},${lastname},${username},${password},${email});`;
+        response.status(201).send({ user: { firstname, lastname, username, email } });
     } catch (e) {
+        console.log(e);
         response.status(500).send({ error: e.message });
     }
 });
@@ -51,11 +68,13 @@ app.post('/signup', async (request, response) => {
 app.post('/post', async (request, response) => {
     try {
         const post = request.body;
-        const sqlPost = await sql`INSERT INTO posts (userID, username, postTitle, postContent,image_url, likes_count, dislikes_count) VALUES
-        ('${post.userID}', '${post.username}', '${post.postTitle}', '${post.postContent}', ${post.image_url}, ${post.likes_count}, '${post.dislikes_count}');`;
+        console.log("post: ", post);
+        const sqlPost = await sql`INSERT INTO posts (userid	, username, postTitle, postContent,image_url, likes_count, dislikes_count) VALUES
+        (${post.userid}, ${post.username}, ${post.postTitle}, ${post.postContent}, ${post.image_url}, ${post.likes_count}, '${post.dislikes_count}');`;
         response.send().status(204);
     } catch (e) {
         response.status(500).send({ error: e.message });
+        console.error(e);
     }
 });
 
@@ -73,15 +92,16 @@ app.delete('/posts/:id', async (request, response) => {
 app.put('/posts/:id', async (request, response) => {
     try {
         const id = +request.params.id;
-        const { title, content, image_url } = request.body;
+        const { title, content, image_url, created_at } = request.body;
         sql = await sql`UPDATE posts SET 
         postTitle = '${title}', 
             content = '${content}', 
-            image_url = '${image_url}';`;
-        
+            image_url = '${image_url}'
+            created_at = '${created_at}'
+            ;`;
+
         response.status(204).send();
     } catch (e) {
-        console.log('e => ', e);
         response.status(500).send({ error: e.message });
     }
 });
@@ -98,11 +118,12 @@ app.get('/books/search', async (request, response) => {
         if (title) {
             filters.push(` postTitle LIKE '%${title}%' `)
         }
-        
-        if(created_at) {
-        filters.push(` created_at = ${created_at}`)}
 
-        if(likeSort) sqlSort += sql` ORDER BY likes_count ${likeSort}`;
+        if (created_at) {
+            filters.push(` created_at = ${created_at}`)
+        }
+
+        if (likeSort) sqlSort += sql` ORDER BY likes_count ${likeSort}`;
         const { rows: posts } = sqlSort
 
         response.send(posts);
